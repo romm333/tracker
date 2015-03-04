30984d13de70692f076b7b9f37137efe8f9d72f6
RomanSkolin@gmail.com <RomanSkolin@gmail.com@e0cff9cf-2839-45f1-ff8f-8b8028f2f6e8>
2015-03-04 20:21:49 +0000

    slight changes

--- src/usertracking/prototype/classes/SimpleTracker.java
+++ src/usertracking/prototype/classes/SimpleTracker.java
@@ -75,9 +75,13 @@ public class SimpleTracker extends Observable {
 
 			userGen.getLostUserEvent().addObserver(new LostUserObserver());
 			
+			userGen.getUserReenterEvent().addObserver(new UserReenterObserver());
+			
 			
 			skeletonCap.getCalibrationCompleteEvent().addObserver(
 					new CalibrationCompleteObserver());
+			poseDetectionCap.getPoseDetectedEvent().addObserver(
+					new PoseDetectedObserver());
 			
 			calibPose = skeletonCap.getSkeletonCalibrationPose();
 			joints = new HashMap<Integer, HashMap<SkeletonJoint, SkeletonJointPosition>>();
@@ -211,7 +215,21 @@ public class SimpleTracker extends Observable {
 	public void setInRecordingMode(boolean isInRecordingMode) {
 		this.isInRecordingMode = isInRecordingMode;
 	}
-
+	
+	class UserReenterObserver implements IObserver<UserEventArgs> {
+		@Override
+		public void update(IObservable<UserEventArgs> observable,
+				UserEventArgs args) {
+			System.out.println("Reentering user " + args.getId());
+					
+			try {
+				skeletonCap.requestSkeletonCalibration(args.getId());
+			} catch (StatusException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}}
+	
 	class UserExitObserver implements IObserver<UserEventArgs> {
 		@Override
 		public void update(IObservable<UserEventArgs> observable,
@@ -219,6 +237,15 @@ public class SimpleTracker extends Observable {
 			System.out.println("Exiting user " + args.getId());
 			profiledUsers.remove((Integer)args.getId());
 			recognizedUsers.remove((Integer)args.getId());
+			
+			try {
+				skeletonCap.clearSkeletonCalibrationData(args.getId());
+				skeletonCap.stopTracking(args.getId());
+				joints.remove((Integer)args.getId());
+			} catch (StatusException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -258,11 +285,12 @@ public class SimpleTracker extends Observable {
 				IObservable<CalibrationProgressEventArgs> observable,
 				CalibrationProgressEventArgs args) {
 
-			// System.out.println("Calibration complete: " + args.getStatus());
+			System.out.println("Calibration complete: " + args.getStatus());
 			try {
 				if (args.getStatus() == CalibrationProgressStatus.OK) {
 
 					System.out.println("starting tracking " + args.getUser());
+					
 					skeletonCap.startTracking(args.getUser());
 					joints.put(new Integer(args.getUser()),
 							new HashMap<SkeletonJoint, SkeletonJointPosition>());
@@ -286,19 +314,207 @@ public class SimpleTracker extends Observable {
 	}
 
 	class PoseDetectedObserver implements IObserver<PoseDetectionEventArgs> {
+		public  double getLenght(Point3D joint1, Point3D joint2) {
+			return Math.sqrt(Math.pow(joint1.getX() - joint2.getX(), 2)
+					+ Math.pow(joint1.getY() - joint2.getY(), 2)
+					+ Math.pow(joint1.getZ() - joint2.getZ(), 2));
+		}
+		
+		
+		public  Point3D getTriangleCentroid(Point3D joint1, Point3D joint2,
+				Point3D joint3) {
+			Point3D retPoint = new Point3D();
+			
+			float X = (joint1.getX() + joint2.getX() + joint3.getX()) / 3;
+			float Y = (joint1.getY() + joint2.getY() + joint3.getY()) / 3;
+			float Z = (joint1.getZ() + joint2.getZ() + joint3.getZ()) / 3;
+
+			retPoint.setPoint(X, Y, Z);
+			return retPoint;
+		}
+		
 		@Override
 		public void update(IObservable<PoseDetectionEventArgs> observable,
 				PoseDetectionEventArgs args) {
-			 System.out.println("Pose " + args.getPose() + " detected for "
-			 + args.getUser());
-			 profiledUsers.add(args.getUser());
+			
+			 try {
+				 
+				 System.out.println("Pose " + args.getPose() + " detected for "
+						 + args.getUser());
+						 profiledUsers.add(args.getUser());
+						 
+						 
+						 /****/
+						 HashMap<SkeletonJoint, SkeletonJointPosition> dict = joints.get(args.getUser());
+							if (dict.size() > 0) {
+
+								SkeletonJointPosition headPosition = dict
+										.get(SkeletonJoint.HEAD);
+								SkeletonJointPosition neckPosition = dict
+										.get(SkeletonJoint.NECK);
+								SkeletonJointPosition torsoPosition = dict
+										.get(SkeletonJoint.TORSO);
+
+								SkeletonJointPosition leftShoulder = dict
+										.get(SkeletonJoint.LEFT_SHOULDER);
+								SkeletonJointPosition rightShoulder = dict
+										.get(SkeletonJoint.RIGHT_SHOULDER);
+
+								SkeletonJointPosition leftHip = dict
+										.get(SkeletonJoint.LEFT_HIP);
+								SkeletonJointPosition rightHip = dict
+										.get(SkeletonJoint.RIGHT_HIP);
+
+								SkeletonJointPosition leftKnee = dict
+										.get(SkeletonJoint.LEFT_KNEE);
+								SkeletonJointPosition rightKnee = dict
+										.get(SkeletonJoint.RIGHT_KNEE);
+
+								SkeletonJointPosition leftElbow = dict
+										.get(SkeletonJoint.LEFT_ELBOW);
+								SkeletonJointPosition rightElbow = dict
+										.get(SkeletonJoint.RIGHT_ELBOW);
+
+								SkeletonJointPosition leftCollar = dict
+										.get(SkeletonJoint.LEFT_COLLAR);
+								SkeletonJointPosition rightCollar = dict
+										.get(SkeletonJoint.LEFT_COLLAR);
+
+								SkeletonJointPosition leftAnkle = dict
+										.get(SkeletonJoint.LEFT_ANKLE);
+								SkeletonJointPosition rightAnkle = dict
+										.get(SkeletonJoint.RIGHT_ANKLE);
+
+								SkeletonJointPosition lehtHand = dict
+										.get(SkeletonJoint.LEFT_HAND);
+								SkeletonJointPosition rightHand = dict
+										.get(SkeletonJoint.RIGHT_HAND);
+								
+								SkeletonJointPosition rightFoot = dict
+										.get(SkeletonJoint.RIGHT_FOOT);
+
+								List<SkeletonJointPosition> interestingJoints = new LinkedList<SkeletonJointPosition>();
+								
+								
+								int frameId = depthGen.getFrameID();
+								int buffFrameId = -1;
+								
+								if (torsoPosition.getConfidence() == 1.0 && headPosition.getConfidence() == 1.0 && rightFoot.getConfidence() == 1.0 && leftElbow.getConfidence()== 1.0
+										&& rightElbow.getConfidence()== 1.0 && buffFrameId != frameId) {
+									 
+									Point3D torsoPos = depthGen
+											.convertProjectiveToRealWorld(torsoPosition
+													.getPosition());
+
+									Point3D neckPos = depthGen
+											.convertProjectiveToRealWorld(neckPosition
+													.getPosition());
+									Point3D headPos = depthGen
+											.convertProjectiveToRealWorld(headPosition
+													.getPosition());
+										
+									
+									Point3D rightHipPos = depthGen
+											.convertProjectiveToRealWorld(rightHip
+													.getPosition());
+
+									Point3D leftHipPos = depthGen
+											.convertProjectiveToRealWorld(leftHip
+													.getPosition());
+									
+									Point3D rightKneePos = depthGen
+											.convertProjectiveToRealWorld(rightKnee
+													.getPosition());
+									Point3D leftKneePos = depthGen
+											.convertProjectiveToRealWorld(leftKnee
+													.getPosition());
+			
+									Point3D rightFootPos = depthGen
+											.convertProjectiveToRealWorld(rightFoot
+													.getPosition());
+			
+									Point3D rightShoulderPos = depthGen
+											.convertProjectiveToRealWorld(rightShoulder
+													.getPosition());
+
+									Point3D leftShoulderPos = depthGen
+											.convertProjectiveToRealWorld(leftShoulder
+													.getPosition());
+									
+									Point3D leftElbowPos = depthGen
+											.convertProjectiveToRealWorld(leftElbow
+													.getPosition());
+									
+									Point3D rightElbowPos = depthGen
+											.convertProjectiveToRealWorld(rightElbow
+													.getPosition());
+									
+									Point3D leftHandPos = depthGen
+											.convertProjectiveToRealWorld(lehtHand
+													.getPosition());
+									
+									Point3D rightHandPos = depthGen
+											.convertProjectiveToRealWorld(rightHand
+													.getPosition());
+									
+									double shoulders = getLenght(rightShoulderPos, leftShoulderPos);
+									double torsoLS = getLenght(torsoPos, leftShoulderPos);
+									double torsoRS = getLenght(torsoPos, rightShoulderPos);
+									
+									double leftShoulderToElbow = getLenght(leftShoulderPos, leftElbowPos);
+									double lefttShoulderToHand = getLenght(leftShoulderPos, leftHandPos);
+									double leftHandToShoulder = getLenght(leftShoulderPos, leftHandPos);
+									
+									double rightShoulderToElbow = getLenght(rightShoulderPos, rightElbowPos);
+									double rightShoulderToHand = getLenght(rightShoulderPos, rightHandPos);
+									double rightHandToShoulder = getLenght(rightShoulderPos, rightHandPos);
+									
+									double rightKneeToHip = getLenght(rightHipPos, rightKneePos);
+									double leftKneeToHip = getLenght(leftHipPos, leftKneePos);
+									
+									Point3D TC = getTriangleCentroid(torsoPos,leftShoulderPos,rightShoulderPos);
+									Point3D LSHC = getTriangleCentroid(leftShoulderPos,leftElbowPos,leftHandPos);
+									Point3D RSHC = getTriangleCentroid(rightShoulderPos,rightElbowPos,rightHandPos);
+									
+									Point3D TLSLS = getTriangleCentroid(torsoPos,leftShoulderPos,leftHipPos);
+									Point3D TRSRH = getTriangleCentroid(torsoPos,rightShoulderPos,rightHipPos);
+									
+
+									Point3D TH = getTriangleCentroid(torsoPos,rightHipPos,leftHipPos);
+									
+									double TC_TH = getLenght(TC, TH);
+									double TLSLS_TRSRH = getLenght(TLSLS, TRSRH);
+									
+									
+									
+									double TC_LSHC = getLenght(TC, LSHC);
+									double TC_RSHC = getLenght(TC, RSHC);
+									double LSHC_RSHC = getLenght(LSHC,RSHC);
+									
+									
+										System.out.println(args.getUser() + ", " + TC_TH + ", " + shoulders + ", " + TLSLS_TRSRH);
+										//System.out.println(users[i] + ", " + torsoLS + ", " + torsoRS + ", " + shoulders + ", " +  rightKneeToHip + ", " + leftKneeToHip);
+									
+
+							}
+						 
+			
+						 
+						 /***/
+						 
+				//poseDetectionCap.stopPoseDetection(args.getUser());
+								}}
+			 catch (StatusException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			 
 			//try {
-//				poseDetectionCap.stopPoseDetection(args.getUser());
+//				
 //				skeletonCap.requestSkeletonCalibration(args.getUser(), true);
 //			} catch (StatusException e) {
 //				e.printStackTrace();
 //			}
-		}
+		}	
 	}
 }
